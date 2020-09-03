0d29439de3b393d0d934a64d0be35fc183a2de21
岡田佳佑 <k-okada@cmind-co.jp>
2020-09-03 13:29:31 +0900

    第2章 - Ruby実践 - 第2部 - Rubyの基本的なデータ型をマスターしていく

---
+++ 2-2.rb
@@ -0,0 +1,30 @@
+# 課題1
+puts "初めてのRuby"
+# 課題2
+puts "Ruby" + "始めました"
+# 課題3
+puts 3 + 3
+puts 3 - 3
+puts 3 * 3
+puts 3 / 3
+# 課題4
+num = [1, 2, 3, 4]
+# 課題5
+num_array = num
+# 課題6
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+# 課題7
+introduction = { name: 'Keisuke okada', birthday: '1995年7月3日', blood: 'O型'}
+# 課題8
+def plus_ruby (name)
+    name + "ruby"
+end
+p plus_ruby('like')
+# 課題9
+def plus_one(num)
+    num + 1
+end
+p plus_one(4)
